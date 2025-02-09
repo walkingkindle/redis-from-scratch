1c3d1e5a5840524693b05e6d8c14f8b82bdbb52e
Aleksa Hadzic <aki.hadza@gmail.com>
2025-02-09 17:20:27 +0100

    interfacing

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using codecrafters_redis;
+
+namespace Main;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)=>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<Worker>();
+            });
+    }
---
+++ Worker.cs
@@ -0,0 +1,27 @@
+﻿using Main;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace codecrafters_redis
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var server = new TCPServer();
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at :{time}", DateTimeOffset.Now);
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
